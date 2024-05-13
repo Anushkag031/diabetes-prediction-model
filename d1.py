640537dbf7f91c39628719a980693dff2a81e0ed
Anushka gupta <anushkagupta319@gmail.com>
2024-05-13 11:19:05 +0530

    first commit

---
+++ d1.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+
+#data collection and analysis
+#loading the datsets
+
+diabetes = pd.read_csv('/Users/anushkag031/Documents/machinelearning/diab-pred/diabetes.csv')
+
+#printing the rows
+print(diabetes.head())
+print(diabetes.shape)
